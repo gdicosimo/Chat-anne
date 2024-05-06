7c510229f838ae02d98752c84c2bf0fc9d02d223
Leonel_Leguizamon <leoleguizamon02@gmail.com>
2024-05-06 10:55:31 -0300

    Correcciones de diseño

--- frontend/src/components/PanelDesplegado/ButtonReg.jsx
+++ frontend/src/components/PanelDesplegado/ButtonReg.jsx
@@ -2,14 +2,13 @@ import React from "react";
 
 const ButtonReg = () => {
     return(
-        <div className="flex flex-col py-20 items-center">
-            <div className='gap-2 text-center flex flex-row rounded-xl bg-white py-3 px-10 flex flex-row text-sm font-poppins font-semibold text-color-black'>
-            Sign up
-          </div>
-          <div className='gap-2 text-center flex flex-row m-5 rounded-xl bg-color-lightblack/20 ring-white ring-2 py-3 px-10 flex flex-row text-sm font-poppins font-semibold text-white'>
+        <div className="flex flex-col py-2 items-center mx-4 gap-2">
+            <button className='hover:opacity-80 hover:scale-[0.99] transition-all w-full justify-center text-center rounded-2xl bg-color-cream py-5 px-10 flex text-sm font-poppins font-semibold text-color-black'>
+              Sign up
+            </button>
+          <button className='hover:opacity-80 hover:scale-[0.99] transition-all w-full justify-center text-center rounded-2xl bg-color-cream/10 border-2 border-color-cream py-5 px-10 flex flex-row text-sm font-poppins font-semibold text-color-cream'>
             Log in
-          </div>
-            
+          </button>
         </div>
     )
 
--- frontend/src/components/PanelDesplegado/Panel.jsx
+++ frontend/src/components/PanelDesplegado/Panel.jsx
@@ -22,17 +22,19 @@ const Panel = ({setButtonOpen}) => {
       return null
     }
     return (
-      <div className='panel space-y-20 pclarge:h-lvh h-dvh'>  
-        <div>
-          <div>
+      <div className='panel flex flex-col justify-between pclarge:h-lvh h-dvh '>  
+        <div className="flex flex-col w-full gap-3">
               <button onClick={closePanel}><img src={cross} className='gap-2 items-center flex flex-row px-3 py-3 m-4 rounded-xl bg-color-lightblack '/></button> 
-          </div>
-            <div>
-              <button onClick={openhistorial} className='gap-2 items-center flex m-4 rounded-xl bg-color-lightblack py-5 px-20 w-96'><img src={add} /> <h4>New chat</h4></button>
-              <button onClick={newChat} className='container-panel'><img src={chat} /> <h4>Show all chats</h4> </button>
-            </div>
+              <button onClick={openhistorial} className='container-panel'>
+                <img src={add} /> 
+                <h1 className="text-base font-poppins font-medium">New chat</h1>
+              </button>
+              <button onClick={newChat} className='container-panel'>
+                <img src={chat} /> 
+                <h1 className="text-base font-poppins font-medium">Show all chats</h1> 
+              </button>
         </div>
-        <div>
+        <div className="flex flex-col h-min">
           {logged ? <ButtonLogOut/>:<ButtonReg/>}
         </div>
       </div> 
