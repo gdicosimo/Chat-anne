bb445dd10f6f58b302d3c653416debf0a0128f23
Leonel_Leguizamon <leoleguizamon02@gmail.com>
2024-05-11 12:53:21 -0300

    nueva funcion de fetching

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, redirect } from 'react-router-dom'
+import { useState } from 'react';
+
 const Login = (props) => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
   
     const handleLogin = async () => {
       try {
-        const response = await fetch('http://127.0.0.1:5000/auth/login/', {
-          mode: 'no-cors',
+        const response = await fetch('http://localhost:5000/auth/login/', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
@@ -28,25 +29,54 @@ const Login = (props) => {
   
     const handleRegister = async () => {
       try {
-        const response = await fetch('http://127.0.0.1:5000/auth/register/', {
-          mode: 'no-cors',
+        console.log(password)
+        console.log(email)
+        const response = await fetch('http://localhost:5000/auth/register/', {
           method: 'POST',
+          withCredentials:true,
+          credentials:'include',
           headers: {
             'Content-Type': 'application/json',
           },
           body: JSON.stringify({ "username": email, "pwd": password }),
         });
-  
+        
         if (response.ok) {
           console.log('Registration successful');
         } else {
           console.error('Registration failed');
-          console.log(response)
+          console.log(response.status)
+          
         }
       } catch (error) {
         console.error('Error occurred:', error);
       }
     };
+
+    function fetch_data6(){
+      console.log("fetching data 2")
+      fetch('http://localhost:5000/auth/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        withCredntials: true,
+        credentials: 'include',
+        body:  JSON.stringify({"username": email,"pwd": password}),
+      })
+      .then((res) => {
+          console.log(res); 
+          res.json().then(
+            (data) => {
+              console.log(data); 
+              redirect("/")
+            }
+          )
+        }
+      )
+      
+  
+    }
   return (
     <div className='bg-color-black pclarge:h-lvh h-dvh flex flex-col items-center justify-between'>
 
@@ -77,8 +107,9 @@ const Login = (props) => {
                     />
                 </div>
                 <div className='mt-8 flex flex-col gap-y-4'>
-                    <Link to='/' 
-                    onClick={props.login ? handleLogin : handleRegister}
+                    <Link 
+                    
+                    onClick={props.login ? ()=>{fetch_data6()} : ()=>{fetch_data6()}}
                     className='active:scale-[.98] active:duration-75 transition-all hover:scale-[0.99] 
                     hover:opacity-95 ease-in-out transform py-4 bg-color-cream rounded-xl text-black 
                     font-bold text-lg text-center'>
