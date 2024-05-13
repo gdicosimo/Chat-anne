7209a52b78d4e3480492fb100d3b5244735c0054
Leonel_Leguizamon <leoleguizamon02@gmail.com>
2024-05-13 14:34:13 -0300

    Sincronizacion automatica docker en frontend

--- frontend/vite.config.mjs
+++ frontend/vite.config.mjs
@@ -5,7 +5,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-      port: 4200,
-      host: true,
+    port: 4200,
+    host: true,
+    watch: {
+      usePolling: true
+    }
   }
 })
