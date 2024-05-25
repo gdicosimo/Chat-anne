0d4bbd8a69b4b9152a25e89b987f5f1a8c281496
BabyBlue <gonzalodicosimo@gmail.com>
2024-05-25 19:53:30 -0300

    Correccion de background en produccion y mejora del setup

--- setup.sh
+++ setup.sh
@@ -2,10 +2,11 @@
 
 ROOT_DIR=$(pwd)
 
+
 get_api_key() {
-    ENV_FILE="${ROOT_DIR}/backend/src/rag/model/.env"
-    if [[ -f "$ENV_FILE" ]]; then
-        existing_api_key=$(grep -oP 'GOOGLE_API_KEY=\K.*' "$ENV_FILE")
+    local env_file="${ROOT_DIR}/backend/src/rag/model/.env"
+    if [[ -f "$env_file" ]]; then
+        local existing_api_key=$(grep -oP 'GOOGLE_API_KEY=\K.*' "$env_file")
         if [[ -n "$existing_api_key" ]]; then
             echo "Ya existe una API key."
             read -p "¿Quieres usar la API Key existente? (y/n): " use_existing
@@ -21,17 +22,18 @@ get_api_key() {
     if [[ -z "$api_key" ]]; then
         echo "No has ingresado ninguna API Key!"
         echo "No podrás ejecutar el backend sin una API Key válida."
-    else
-        if [[ "$api_key" =~ [[:space:]] ]]; then
-            echo "La API Key no puede contener espacios en blanco!"
-            echo "No podrás ejecutar el backend sin una API Key válida."
-        else
-            echo "GOOGLE_API_KEY=$api_key" > "$ENV_FILE"
-            echo "Se ha creado el archivo .env con la API Key."
-        fi
+        exit 1
     fi
-}
 
+    if [[ "$api_key" =~ [[:space:]] ]]; then
+        echo "La API Key no puede contener espacios en blanco!"
+        echo "No podrás ejecutar el backend sin una API Key válida."
+        exit 1
+    fi
+
+    echo "GOOGLE_API_KEY=$api_key" > "$env_file"
+    echo "Se ha creado el archivo .env con la API Key."
+}
 
 install_pip_dependencies() {
     local project_directory=$1
@@ -69,14 +71,39 @@ show_spinner() {
     local pid=$1
     local delay=0.1
     local spinstr='|/-\'
-    while [ "$(ps a | awk '{print $1}' | grep -w $pid)" ]; do
+    while ps -p $pid &> /dev/null; do
         local temp=${spinstr#?}
         printf " [%c]  " "$spinstr"
-        local spinstr=$temp${spinstr%"$temp"}
+        spinstr=$temp${spinstr%"$temp"}
         sleep $delay
         printf "\b\b\b\b\b\b"
     done
     printf "    \b\b\b\b"
+}
+
+check_docker_engine() {
+    if ! docker info &> /dev/null; then
+        echo "Docker Engine no está corriendo. Intentando iniciarlo..."
+        if command -v dockerd &> /dev/null; then
+            nohup sudo dockerd &> /dev/null &
+            sleep 5
+            if ! docker info &> /dev/null; then
+                echo "No se pudo iniciar Docker Engine. Por favor, inicia Docker manualmente y vuelve a intentar."
+                exit 1
+            else
+                echo "Docker Engine iniciado exitosamente."
+            fi
+        else
+            echo "dockerd no encontrado. Por favor, asegúrate de que Docker esté instalado correctamente."
+            exit 1
+        fi
+    fi
+}
+
+open_docker_desktop() {
+    echo "Opening Docker Desktop..."
+    docker_desktop_path=$(powershell.exe -Command "(Get-Process 'Docker Desktop').Path")
+    start "$docker_desktop_path"
 }
 
 
@@ -95,45 +122,55 @@ run_docker() {
     fi
 }
 
-read -p "¿Quieres instalar las dependencias del proyecto localmente? (y/n): " install_local
+main() {
+    read -p "¿Quieres instalar las dependencias del proyecto localmente? (y/n): " install_local
 
-if [[ "$install_local" =~ ^[Yy]$ ]]; then
-    # Check if Python command is available
-    if command -v python3 &> /dev/null || command -v py &> /dev/null ; then
-        echo "Python encontrado."
-        if [ -d "backend" ]; then
-            install_pip_dependencies "backend"
+    if [[ "$install_local" =~ ^[Yy]$ ]]; then
+        # Check if Python command is available
+        if command -v python3 &> /dev/null || command -v py &> /dev/null; then
+            echo "Python encontrado."
+            if [ -d "backend" ]; then
+                install_pip_dependencies "backend"
+            else
+                echo "Error: directorio 'backend' no encontrado."
+                exit 1
+            fi
         else
-            echo "Error: directorio 'backend' no encontrado."
+            echo "Python no encontrado. Saliendo."
+            exit 1
+        fi
+
+        # Check if npm command is available
+        if command -v npm &> /dev/null; then
+            echo "npm encontrado."
+            if [ -d "frontend" ]; then
+                install_node_dependencies "frontend"
+            else
+                echo "Error: directorio 'frontend' no encontrado."
+                exit 1
+            fi
+        else
+            echo "npm no encontrado. Por favor, instala Node.js y npm para continuar."
+            exit 1
         fi
     else
-        echo "Python no encontrado. Saliendo."
-        exit 1
+        echo "No se instalarán las dependencias localmente."
     fi
 
-    # Check if npm command is available
-    if command -v npm &> /dev/null; then
-        echo "npm encontrado."
-        if [ -d "frontend" ]; then
-            install_node_dependencies "frontend"
-        else
-            echo "Error: directorio 'frontend' no encontrado."
+    get_api_key
+    check_docker_engine
+
+    read -p "¿Quieres ejecutar el proyecto? (y/n): " run_project
+    if [[ "$run_project" =~ ^[Yy]$ ]]; then
+        read -p "¿En qué modo quieres ejecutar el proyecto? (dev/prod): " mode
+        if [[ $(uname) == "MINGW"* ]]; then
+            open_docker_desktop
         fi
-    else
-        echo "npm no encontrado. Por favor, instala Node.js y npm para continuar."
-        exit 1
+        run_docker "$mode"
     fi
-else
-    echo "No se instalarán las dependencias localmente."
-fi
 
-get_api_key
+    read -rp "Presiona Enter para salir..." input
+}
 
-read -p "¿Quieres ejecutar el proyecto? (y/n): " run_project
-if [[ "$run_project" =~ ^[Yy]$ ]]; then
-    read -p "¿En qué modo quieres ejecutar el proyecto? (dev/prod): " mode
-    run_docker "$mode"
-fi
-
-read -rp "Presiona Enter para salir..." input
+main
 
